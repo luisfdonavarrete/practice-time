c18e5814432798dbcf106cd2ee3f637f021a99e7
Luis Navarrete <luisfdonavarrete@users.noreply.github.com>
2026-08-05 17:45:12 -0400

    added global interceptor to format api responses

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './app-config/app.config.service';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalResponseInterceptor } from './common/api/interceptors/global-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.useGlobalInterceptors(new GlobalResponseInterceptor());
 
   await app.listen(configService.port);
 }
